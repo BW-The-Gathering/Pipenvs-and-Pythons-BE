1df486ad284b0e02ac8d51ce54a359a96543fbfd
Ahmad Guenoun <amguenoun@gmail.com>
2020-04-01 14:22:10 -0400

    created mutation for adding users
    Co-authored-by: srfinley <samantharfinley@gmail.com>

--- adventure/schema.py
+++ adventure/schema.py
@@ -1,7 +1,29 @@
 import graphene
 from graphene_django.types import DjangoObjectType
+from django.contrib.auth import get_user_model
 from .models import Player, Map, Room
 
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+    
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
 
 class PlayerType(DjangoObjectType):
     class Meta:
@@ -88,5 +110,6 @@ class Query(graphene.ObjectType):
 
 class Mutation(graphene.ObjectType):
     player_mutation = PlayerMutation.Field()
+    create_user = CreateUser.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
