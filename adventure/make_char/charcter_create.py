3e18da98d12e8499d9f4f4c4e7dad79078781bdb
hakujouryu <hakujouryu@gmail.com>
2020-04-02 13:20:19 -0400

    Create startGame in schema
    Co-authored-by: srfinley <samantharfinley@gmail.com>
    Co-authored-by: Phatdeluxe <ethan@skamarock.net>

--- adventure/make_char/charcter_create.py
+++ adventure/make_char/charcter_create.py
@@ -1,7 +1,7 @@
 from adventure.make_world.MakeRoom import make_player, make_player_old, make_map, make_rooms
 
 
-def pc(user):
+def game_start(user):
     """Generates a new player, map, and populates map with rooms. 
     Sets player's positions  
     
@@ -9,19 +9,19 @@ def pc(user):
         request  -- request packet
     
     Returns:
-        Dict -- Returns User, Player, 
+        Dict -- Returns Player, new_map, and rooms in map
     """
 
     username = user.username
-    player = make_player(user_id=request.user)
+    player = make_player(user_id=user)
     new_map = make_map(player)
-    rooms = make_rooms(new_map)
+    rooms = make_rooms(new_map, genre="fantasy")
     player.position = rooms[(0,0)]
+    player.save()
 
     return_packet = {
-        "user": user,
         "player": player,
         "map_id": new_map, 
         "rooms": rooms
     }
-    return return_packet
+    return player
