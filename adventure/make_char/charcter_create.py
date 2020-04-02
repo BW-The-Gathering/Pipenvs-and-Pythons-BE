e19c9b1d44122a4e8c52b4d46beef16de59510dc
Samantha Finley <srfinley@uchicago.edu>
2020-04-02 18:26:33 -0400

    migration for adding end to room model plus logic for inserting end at either first or last room

--- adventure/make_char/charcter_create.py
+++ adventure/make_char/charcter_create.py
@@ -1,5 +1,6 @@
 from adventure.make_world.MakeRoom import make_player, make_player_old, make_map, make_rooms
 
+from adventure.models import Room
 
 def game_start(user):
     """Generates a new player, map, and populates map with rooms. 
@@ -18,6 +19,15 @@ def game_start(user):
     rooms = make_rooms(new_map, genre="fantasy")
     player.position = rooms[(0,0)]
     player.save()
+
+    # new content
+    room_count = len(Room.objects.filter(map_id=new_map.id))
+    last_room = Room.objects.filter(map_id=new_map.id).order_by('id')[room_count-1]
+    # FOR PRODUCTION: ^ this line puts the end at the last room created
+    # last_room = Room.objects.filter(map_id=new_map.id).order_by('id')[1]
+    # FOR TESTING: ^ this line puts the end at the second room of the map (north of entrance)
+    last_room.end = True
+    last_room.save()
 
     return_packet = {
         "player": player,
